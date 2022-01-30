29ea262176ead08de95dbbd3cf418c14fbf98259
Edward Hurtado <edhur9@gmail.com>
2022-01-30 00:14:40 -0500

    beginning

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+//variables
+const c1 = document.querySelector('.c1');
+const c2 = document.querySelector('.c2');
+const c3 = document.querySelector('.c3');
+const c4 = document.querySelector('.c4');
+const c5 = document.querySelector('.c5');
+const c6 = document.querySelector('.c6');
+const c7 = document.querySelector('.c7');
+const c8 = document.querySelector('.c8');
+const c9 = document.querySelector('.c9');
+const mempattern_btn = document.querySelector('.pattern_login_button');
+let arr_of_circles = [c1, c2, c3, c4, c5, c6, c7, c8, c9];
+
+//functions
+
+//event listeners
+mempattern_btn.addEventListener('click', function () {
+  // arr_of_circles.forEach(c => {
+  //   c.classList.add('animation');
+  // });
+  // let i = Math.trunc(Math.random() * 9 + 1);
+});
